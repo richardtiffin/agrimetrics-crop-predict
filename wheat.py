6a5202cc8b9c3c7c83c7aa42016f15e9e57074b4
richard tiffin <rich.tiffin@gmail.com>
2018-10-08 13:24:18 +0100

    count wheat fields

--- wheat.py
+++ wheat.py
@@ -7,25 +7,29 @@ headers = {
 }
 baseuri = 'https://api.agrimetrics.co.uk'
 names = helper.getPlantNames(baseuri, headers)
-nres = 10000
-skip = 1000
+nres = 14000000
+skip = 1000000
 
 plant='Wheat'
-savedcrops=[]
+savedcrops2016 = []
+savedcrops2017 = []
 for i in range(0,nres-skip + 10,skip):
     print(i)
-    ODataQuery = "?$filter=Field/hasSownCrop/any(c: c/label eq \'" + plant + "\' and c/harvestYear eq 2016)&$select=Field/hasSownCrop&$top=1000&$skip=" + str(i)
+    ODataQuery = "?$filter=Field/hasRepresentativePoint/altitude gt 0&$select=Field/hasSownCrop&$top=1000&$skip=" + str(i)
     fieldlist = helper.fieldSearch(ODataQuery, baseuri, headers)
 
     try:
         for field in fieldlist['results']:
             for year in field['hasSownCrop']:
                 if year['harvestYear'] == 2017:
-                        savedcrops.append(year['label'])
+                        savedcrops2017.append(year['label'])
+                if year['harvestYear'] == 2016:
+                    savedcrops2016.append(year['label'])
     except:
         pass
 
 
 # print(len(fieldlist['results']))
 # pprint(fieldlist)
-pprint(savedcrops)
+pprint(savedcrops2016.count('Wheat')/len(savedcrops2016))
+pprint(savedcrops2017.count('Wheat')/len(savedcrops2017))
